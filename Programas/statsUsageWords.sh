00a9b847814cffad3de6ce34ade7930bb2abf270
Pablo Pistarelli <ppistarelli@gmail.com>
2022-08-15 11:36:17 -0300

    modificados:     statsUsageWords.sh 	borrados:        ../B.txt 	modificados:     ../archivo.txt

--- Programas/statsUsageWords.sh
+++ Programas/statsUsageWords.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-ORDENADOFILTRADO=$(cat archivo.txt | tr " " "\n" | tr -d '.' | tr -d ',' | sort | grep '....' | uniq -i -c | sort -r)
+ORDENADOFILTRADO=$(cat archivo.txt | tr " " "\n" | tr -d '.' | tr -d ',' | tr -d '?' | sort | grep '....' | uniq -i -c | sort -r)
 I=0
 for P in $ORDENADOFILTRADO
 do
-	if (($I > 10))
+	if (($I == 20))
 	then
 		break
 	else
 	echo $P
-	fi	
+	I=$((I+1))
+	fi 	
 done
 
 
